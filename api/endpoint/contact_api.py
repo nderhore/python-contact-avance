8f1b2522b141145985867ee0b1455ac49a3f0fff
Nathan <nathan@derhore.fr>
2023-06-01 21:11:33 +0200

    jinja2 fastapi

--- api/endpoint/contact_api.py
+++ api/endpoint/contact_api.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 from core.config.database import get_db
 from core.models.contact import Contact
-from core.schema.ContactSchema import ContactCreate
+from core.schema.ContactSchema import ContactCreate, ContactUpdate
 from core.schema.ContactSchema import Contact as contactPydantic
 
 router = APIRouter()
 
 
-@router.post("/", response_model=contactPydantic)
+@router.post("/")
 async def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
     db_contact = Contact()
     db_contact.mail = contact.mail
@@ -18,4 +19,27 @@ async def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
     db_contact.telephone = contact.telephone
     db.add(db_contact)
     db.commit()
-    db.refresh()
+    db.refresh(db_contact)
+    return JSONResponse(content={"message": "Intégré"})
+
+@router.get("/{id}",response_model=contactPydantic)
+async def get_contact_by_id(contact_id: int, db: Session = Depends(get_db)) -> contactPydantic:
+   return db.query(Contact).filter(Contact.id == contact_id).first()
+
+@router.get("/", response_model=list[contactPydantic])
+async def get_contacts(db: Session = Depends(get_db)):
+    return db.query(Contact).all()
+
+@router.put("/{contact_id}",response_class=JSONResponse)
+async def update_contact_by_id(contact_id : int, contact: ContactUpdate, db : Session = Depends(get_db)):
+    old_contact = get_contact_by_id(contact_id)
+    if old_contact:
+        old_contact.nom = contact.nom
+        old_contact.mail = contact.mail
+        old_contact.telephone = contact.telephone
+        old_contact.prenom = contact.prenom
+        db.commit()
+        db.refresh(old_contact)
+        return JSONResponse(content={"message": "Contact mis à jour avec succès", "contacts" : old_contact})
+    return JSONResponse(content={"message":"Contact non trouvé."})
+
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 import api.api as api
+from core.config.database import Base, engine
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 
 origins = ["http://localhost:4200"]
 
@@ -16,4 +19,30 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "contacts": [
+  {
+    "nom": "Marushka",
+    "prenom": "Sergio",
+    "telephone": "3630",
+    "mail": "toto@jose.fr"
+  },
+  {
+    "nom": "toto",
+    "prenom": "titi",
+    "telephone": "tutu",
+    "mail": "tata"
+  },
+  {
+    "nom": "de",
+    "prenom": "de",
+    "telephone": "d",
+    "mail": "de"
+  }
+]})
+
+
 app.include_router(api.router, prefix="/api")
+Base.metadata.create_all(bind=engine)
